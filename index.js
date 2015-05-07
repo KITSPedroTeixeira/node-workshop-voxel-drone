fcfb49d54c895fc5c276af3f487b28ea217e0396
Pedro Teixeira <i@pgte.me>
2015-05-07 10:26:37 +0100

    take off

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(function(req, res, next) {
 
 app.use(express.static(join(__dirname, 'browser')));
 
+
 var server = app.listen(3000, function() {
   console.log('HTTP server listening on %j', server.address());
 });
@@ -17,6 +18,24 @@ var server = app.listen(3000, function() {
 var websocket = require('websocket-stream');
 var wss = websocket.createServer({server: server}, handleWebsocket);
 
+var rpc = require('rpc-stream')();
+
+var commands = require('./browser/commands');
+
+var drone = rpc.wrap(commands);
+
 function handleWebsocket(websocket) {
-  console.log('I HAZ WEBSOCKET');
+  rpc.pipe(websocket).pipe(rpc, {end: false});
 }
+
+app.post('/takeoff', function(req, res) {
+  drone.takeoff(function(err) {
+    if (err) {
+      res.status(500);
+      res.send(err);
+    }
+    else {
+      res.send({ok: true});
+    }
+  });
+});
